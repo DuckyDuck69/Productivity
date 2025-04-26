a8ff5d938f4b570a83adb05f8703842e596da0ed
Truong The Kha <kha@n3-90-4.dhcp.drexel.edu>
2025-04-26 15:49:43 -0400

    Kha Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import TimeDuration from "./components/TimeDuration"
 import Title from "./components/Title"
+import FileInput from "./components/FileInput"
+import TodoList from "./components/ToDoList"
 
 function App() {
   return (
     <>
-      <div className="page-container">
+      <div className="page-container" style = {{display : "inline-flex"}}>
+
         <Title/>
         <TimeDuration/>
+        <FileInput />
+        <TodoList />
       </div>
     </>
   )
