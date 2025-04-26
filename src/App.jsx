2c69b68b5b64169c19d54fd4a34be31f86ba8cd6
DuckyDuck69 <phanthaihoangduc99@gmail.com>
2025-04-26 15:47:43 -0400

    video feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TimeDuration from "./components/TimeDuration"
 import Title from "./components/Title"
+import Video from "./components/Video"
 
 function App() {
   return (
     <>
       <div className="page-container">
         <Title/>
+        <Video/>
         <TimeDuration/>
       </div>
     </>
