fe0a43460c6204f72272f857808c7e51ecbefd8e
Truong The Kha <kha@n3-90-4.dhcp.drexel.edu>
2025-04-26 21:48:21 -0400

    styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Title from "./components/Title"
 import FileInput from "./components/FileInput"
 import TodoList from "./components/ToDoList"
 import Video from "./components/Video"
-
+import StudyMethod from "./components/StudyMethod"
 
 function App() {
+
+
   return (
-      <div className="grid grid-cols-3 gap-4">
-        <div className="col-start-2"><Title/> </div>
-        <div className="col-start-3"><Video/> </div>
-        <div className="col-start-1"> <TodoList /> </div>
-        <div  className="col-start-2 col-span-3"><FileInput/> </div>
-        <div className="col-start-1"><TimeDuration/> </div>
+      <div className="grid grid-cols-3">
+
+        <div className="col-start-2 col-span-3"><Title/></div>
+        <div className="col-start-3"><Video/></div>
+        <div className="col-start-1"><TodoList /></div>
+        <div className="col-start-2 col-span-3"><FileInput /></div>
+        <div className="col-start-1"><TimeDuration/></div>
+        <div className="col-start-1 col-span-2"><StudyMethod/></div>
       </div>
   )
 }
---
+++ src/components/StudyMethod.jsx
@@ -0,0 +1,21 @@
+import { useState, useRef } from "react";
+
+function StudyMethod({method, setMethod }) {
+    function handleChanges(event) {
+    const selectedOne = event.target.value;
+    setMethod(selectedOne);
+    console.log("Choose study: ", selectedOne);
+  }
+  return (
+    <div>
+      <h4>Extra Study Methods: </h4>
+      <select value={method} onChange={handleChanges}>
+        <option value="regular">No Option</option>
+        <option value="pomodoro">Pomodoro</option>
+        <option value="flowtime">Flowtime Technique</option>
+        <option value="ultradian">Ultradian Sprints</option>
+      </select>
+    </div>
+  );
+}
+export default StudyMethod;
