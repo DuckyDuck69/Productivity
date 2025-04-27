66d44100723b4b280c76a76a02a9ecae8cfe2b56
DuckyDuck69 <phanthaihoangduc99@gmail.com>
2025-04-27 03:48:43 -0400

    pulllll

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,8 @@
-function Title(){
-    return(
-        <div className="inline-block p-4 rounded-2xl bg-[#FFCF9D]/90 shadow-md hover:shadow-lg transition-shadow duration-300">
-            <h1 className="text-4xl font-bold text-white">Productivity++</h1>
-        </div>
-    );
-}   
-export default Title
+function Title() {
+  return (
+    <div className="inline-block p-4 rounded-2xl bg-[#FFCF9D]/90 shadow-md hover:shadow-lg transition-shadow duration-300">
+      <h1 className="text-xl font-bold text-white">Productivity++</h1>
+    </div>
+  );
+}
+export default Title;
